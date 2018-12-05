08cbddf46b2c3853bc180c07a2b17233fa2c043d
divya0319 <divygupta0319@gmail.com>
2018-12-05 12:49:35 +0530

    Implemented MVVM using Kotlin

--- app/src/main/java/com/practicesession/databindingkotlinexample/MainActivity.kt
+++ app/src/main/java/com/practicesession/databindingkotlinexample/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.practicesession.databindingkotlinexample
 
-import android.support.v7.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.practicesession.databindingkotlinexample.databinding.ActivityMainBinding
+import com.practicesession.databindingkotlinexample.usermodel.User
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val activityBinding: ActivityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        val user = User("John", "Reed")
+        activityBinding.setVariable(BR.userModel,user)
+
+        activityBinding.executePendingBindings()
+
     }
 }
